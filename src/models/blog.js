7ed1933d0c9c0c92626de78a943ac8bb2459641f
krm22 <krmouat22@gmail.com>
2016-12-19 23:05:10 +0100

    make it better

--- src/models/blog.js
+++ src/models/blog.js
@@ -10,7 +10,7 @@ let mongoose = require('mongoose');
 let blogSchema = new mongoose.Schema({
     title: String,
     author: String,
-    PublishedAt: String,
+    PublishedAt: Date,
     content: String
 });
 
